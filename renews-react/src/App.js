ecbe37b8da118a35740648ffbb53563ca0b3b12b
shweta_sim <shweta.sim@gmail.com>
2020-01-03 21:43:01 +0100

    search component and news card done

--- renews-react/src/App.js
+++ renews-react/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Search from './components/Search';
+import AddCategory from './components/AddCategory';
 import MyTopics from './components/MyTopics';
 import DateTime from './components/DateTime';
 import Weather from './components/Weather';
@@ -7,12 +7,66 @@ import Banner from './components/Banner';
 import NewsCard from './components/NewsCard';
 import Footer from './components/Footer';
 
+// NEWS CONFIG
+const search_url = 'https://gnews.io/api/v3/search';
+const _api_key = '9479c5195f851b70901766087171c584';
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      articles: []
+    };
+    this.handleFetch = this.handleFetch.bind(this);
+    this.getValueByEnter = this.getValueByEnter.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  handleFetch = searchVal => {
+    fetch(`${search_url}?q=${searchVal}&token=${_api_key}`)
+      .then(res => res.json())
+      .then(data => {
+        /*{showCards(data);}*/
+        this.setState({
+          articles: data.articles
+        });
+      })
+      .catch(console.log);
+  };
+
+  getValueByEnter = event => {
+    if (event.key === 'Enter') {
+      const searchVal = document.querySelector('#search').value;
+      this.handleFetch(searchVal);
+    }
+  };
+
+  handleSearch = event => {
+    event.preventDefault();
+    const searchVal = document.querySelector('#search').value;
+    this.handleFetch(searchVal);
+  };
+
   render() {
     return (
       <div>
         <header>
-          <Search />
+          <section className='search'>
+            <input
+              type='text'
+              id='search'
+              name='search'
+              placeholder='What are we looking for?'
+              onKeyDown={this.getValueByEnter}
+            />
+            <section className='searchbtn'>
+              <a href='#' id='searchButton' onClick={this.handleSearch}>
+                <i className='fas fa-search'></i>
+              </a>
+            </section>
+
+            <AddCategory />
+          </section>
         </header>
         <MyTopics />
 
@@ -23,7 +77,19 @@ class App extends Component {
           </aside>
           <Banner />
         </section>
-        <NewsCard />
+
+        <section class='newsfeed'>
+          {this.state.articles.map((article, id) => {
+            return (
+              <NewsCard
+                key={id}
+                title={article.title}
+                description={article.description}
+              />
+            );
+          })}
+        </section>
+
         <Footer />
       </div>
     );
--- renews-react/src/components/NewsCard.js
+++ renews-react/src/components/NewsCard.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function NewsCard() {
-  return <section class='newsfeed'></section>;
+class NewsCard extends Component {
+  render() {
+    return (
+      <article className='newsCard'>
+        <div>
+          <img src='imgs/img-news1.jpg' />
+          <h5>{this.props.title}</h5>
+          <p className='datetime'>29-10-2019 06:30PM</p>
+          <p>{this.props.description}</p>
+        </div>
+      </article>
+    );
+  }
 }
 
 export default NewsCard;
