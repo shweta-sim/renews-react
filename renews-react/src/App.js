8ee1bf13249b7912f293ba57f346c991eca15aa8
shweta_sim <shweta.sim@gmail.com>
2020-01-08 19:19:44 +0100

    banner image from unsplash working

--- renews-react/src/App.js
+++ renews-react/src/App.js
@@ -9,6 +9,7 @@ import Footer from './components/Footer';
 
 // NEWS CONFIG
 const search_url = 'https://gnews.io/api/v3/search';
+const top_news_url = 'https://gnews.io/api/v3/top-news';
 const _api_key = '9479c5195f851b70901766087171c584';
 
 class App extends Component {
@@ -46,6 +47,19 @@ class App extends Component {
     event.preventDefault();
     const searchVal = document.querySelector('#search').value;
     this.handleFetch(searchVal);
+  };
+
+  componentDidMount = () => {
+    // Request for random top news items
+    fetch(`${top_news_url}?token=${_api_key}`)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          articles: data.articles
+        });
+        console.log(data);
+      })
+      .catch(console.log);
   };
 
   render() {
--- renews-react/src/components/Banner.js
+++ renews-react/src/components/Banner.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-function Banner() {
-  return <section class='w-banner'></section>;
-}
+// BANNER CONFIG
+const banner_url =
+  'https://api.unsplash.com/photos/random?orientation=landscape&client_id=';
+const banner_api_key =
+  'fbad019b2bc99126150dbdab0a1abfc55c48f2162ee9e5d6fd991778600ca6ef';
+
+const Banner = () => {
+  const [banner, setBanner] = useState();
+
+  useEffect(() => {
+    fetch(`${banner_url}${banner_api_key}`)
+      .then(res => res.json())
+      .then(data => {
+        setBanner(data.urls.full);
+      })
+      .catch(console.log);
+  }, []);
+
+  return (
+    <section
+      class='w-banner'
+      style={{
+        backgroundImage: `url('${banner}')`
+      }}></section>
+  );
+};
 
 export default Banner;
