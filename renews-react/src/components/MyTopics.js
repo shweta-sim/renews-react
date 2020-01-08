a9c3a32dbfa87cc9e99ce37523d7bedef906a633
shweta_sim <shweta.sim@gmail.com>
2020-01-09 00:48:42 +0100

    categories buttons generated using state

--- renews-react/src/components/MyTopics.js
+++ renews-react/src/components/MyTopics.js
@@ -1,22 +1,31 @@
-import React from 'react';
-
-let defaultTopics = [
-  'world',
-  'nation',
-  'business',
-  'technology',
-  'entertainment',
-  'sports',
-  'science',
-  'health'
-];
+import React, { useState, useEffect } from 'react';
 
 const MyTopics = () => {
+  const [userTopics, setUserTopics] = useState([
+    'world',
+    'nation',
+    'business',
+    'technology',
+    'entertainment',
+    'sports',
+    'science',
+    'health'
+  ]);
+
+  let buttonArray = [];
+
+  const generateButton = () => {
+    buttonArray = userTopics.map(topicName => {
+      return <button>{topicName}</button>;
+    });
+    return buttonArray;
+  };
+
   return (
     <div className='modal'>
       <div className='modal-content'>
         <h1>Choose News Topics</h1>
-        <section id='addTopicsWrapper'></section>
+        <section id='addTopicsWrapper'>{generateButton()}</section>
       </div>
     </div>
   );
